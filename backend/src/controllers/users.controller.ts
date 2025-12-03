d4e5ed0387bd2ae25f7b8c150cabcde96b0dc75d
WesRamox <wesleyramox258@gmail.com>
2025-12-03 13:47:20 -0300

    feat(backend): add user fetching by ID

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -6,6 +6,23 @@ export async function getAllUsers(req: Request, res: Response) {
   return res.json(rows);
 }
 
+export async function getUserById(req: Request, res: Response) {
+  const { id } = req.params
+
+  try {
+    const query = "SELECT * FROM users WHERE id = $1"
+    const result = await db.query(query, [id])
+
+    if (result.rows.length <= 0) {
+      return res.status(404).json({ message: "Usuario não encontrado, por favor forneça um ID válido"})
+    } else {
+      return res.status(200).json({ message: result.rows });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Erro interno inesperado.", error });
+  }
+}
+
 export async function createNewUser(req: Request, res: Response) {
   const { name, phone } = req.body
 
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createNewUser, getAllUsers } from "../controllers/users.controller";
+import { createNewUser, getAllUsers, getUserById } from "../controllers/users.controller";
 
 const router = Router();
 
 router.get("/", getAllUsers);
+router.get("/:id", getUserById)
 router.post("/create", createNewUser)
 
 export default router;
