046487dd42b7c546f9ed393be822ea9e6fbda491
WesRamox <wesleyramox258@gmail.com>
2025-12-15 21:54:49 -0300

    chore(frontend): initialize frontend with Vite

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { signInAuth, signUpAuth } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
